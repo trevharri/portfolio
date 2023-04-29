15afe54b026b849d257958fed099cf5a52bf1914
trevharri <113393597+trevharri@users.noreply.github.com>
2023-04-29 21:37:59 +0200

    Changed button text

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -30,8 +30,8 @@ const Projects = () => (
               </TagList>
             </div>
             <UtilityList>
-              <ExternalLinks href={visit}>Code</ExternalLinks>
-              <ExternalLinks href={source}>Source</ExternalLinks>
+              <ExternalLinks href={visit}>Visit</ExternalLinks>
+              <ExternalLinks href={source}>See the code</ExternalLinks>
             </UtilityList>
           </BlogCard>
         ))
